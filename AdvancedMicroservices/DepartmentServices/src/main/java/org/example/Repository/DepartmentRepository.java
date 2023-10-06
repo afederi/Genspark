704cfa2caa2fbfa21912bfde11d48ef82f5b2e9a
Alex <alex404an@gmail.com>
2023-10-06 11:06:42 -0400

    Updated DepartmentRepository.java

--- AdvancedMicroservices/DepartmentServices/src/main/java/org/example/Repository/DepartmentRepository.java
+++ AdvancedMicroservices/DepartmentServices/src/main/java/org/example/Repository/DepartmentRepository.java
@@ -1,7 +1,10 @@
 package org.example.Repository;
 
+import org.example.Entity.Department;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class DepartmentRepository {
+public interface DepartmentRepository extends JpaRepository<Department, Long> {
+
 }
