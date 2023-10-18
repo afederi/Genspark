c546fd3548bef465cf045476e32552edbd50ce3e
Alex <alex404an@gmail.com>
2023-10-18 10:49:57 -0400

    update project

---
+++ AdvancedMicroservices/AdvancedSecurity/src/main/java/org/example/Controller/MyController.java
@@ -0,0 +1,20 @@
+package org.example.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/home")
+public class MyController {
+    @GetMapping("/normal")
+    public String normalUser(){ return "<HTML><H1> I am a normal user </H1><HTML>";}
+
+    @GetMapping("/admin")
+    public String adminUser(){ return "<HTML><H1> I am a admin user </H1><HTML>";}
+
+    @GetMapping("/public")
+    public String publicUser(){ return "<HTML><H1> I am a public user </H1><HTML>";}
+
+
+}
